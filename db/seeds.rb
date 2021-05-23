78fe6ec3defc6ec07c8c5fa8d2b17b299e38ec18
Boyu <rider945@outlook.com>
2021-05-23 13:57:36 +0800

    feat: add default categories to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+category_data = [
+  { name: 'business' },
+  { name: 'computer_science' },
+  { name: 'data_science' },
+  { name: 'information_technology' },
+  { name: 'social_science' },
+  { name: 'arts_and_humanities' }
+]
+
+category_data.each do |category|
+  object = Category.where(category).first_or_create
+  puts "find or create category id: #{object.id}, name: #{object.name}"
+end
